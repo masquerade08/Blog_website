77fba0e20270493349b07f6da652132e3e098884
raikr <raikrishna882000@gmail.com>
2024-01-29 19:57:50 +0530

    fix code

---
+++ client/src/redux/store.js
@@ -0,0 +1,22 @@
+import { create } from "@mui/material/styles/createTransitions";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isLogin: false,
+  },
+  reducers: {
+    login(state) {
+      state.isLogin = true;
+    },
+    logout(state) {
+      state.isLogin = false;
+    },
+  },
+});
+export const authActions = authSlice.actions;
+
+export const store = configureStore({
+  reducer: authSlice.reducer,
+});
